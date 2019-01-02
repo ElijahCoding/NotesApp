51a2c001a31846fd48c516d6ec2e648c5a829f79
LittleElijah <cuiyeqing960904@gmail.com>
2019-01-02 22:49:32 +0300

    test styles

--- src/App.vue
+++ src/App.vue
@@ -8,3 +8,23 @@ export default {
   name: 'App'
 }
 </script>
+
+<style>
+body, html {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+}
+
+body {
+    color: #2c3e50;
+}
+
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    display: flex;
+    height: 100%;
+}
+</style>
